1b2d39a5958f80151ed4bec7b4ba7a95a135979d
Soumyajit2000-web <soumyajitd2000@gmail.com>
2022-10-31 11:08:47 +0530

    baseUrl fix

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+export const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "https://heliosx.azurewebsites.net/api"
 
 export const api = axios.create({
     baseURL: baseURL
