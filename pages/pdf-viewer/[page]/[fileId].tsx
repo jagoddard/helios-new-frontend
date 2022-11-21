3ecb3bd9977b6beeed3c2814a3359ea470711794
saqquraza <saqlainaman0@gmail.com>
2022-11-21 19:11:30 +0530

    asset

--- pages/pdf-viewer/[page]/[fileId].tsx
+++ pages/pdf-viewer/[page]/[fileId].tsx
@@ -2,142 +2,154 @@ import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { getArticleFileFromBlob } from "../../../services/articles";
-import { getFinancialFileFromBlob, getFinancialMdaFileFromBlob } from "../../../services/financials";
+import {
+  getFinancialFileFromBlob,
+  getFinancialMdaFileFromBlob,
+} from "../../../services/financials";
 import { getInvestorFileFromBlob } from "../../../services/investors";
 import { getFileFromBlob } from "../../../services/news";
 import { getTechnicalFileFromBlob } from "../../../services/technicalReports";
-const BarLoader = dynamic(() => import('react-spinners/BarLoader'), { ssr: false })
+import useWindowSize from "../../../utils/useWindowSize";
+
+const BarLoader = dynamic(() => import("react-spinners/BarLoader"), {
+  ssr: false,
+});
 
 function FilePage() {
   const [preload, setPreload] = useState(false);
+  const windowsize = useWindowSize();
+  console.log(windowsize.width);
+  const width = windowsize.width;
+
   const router = useRouter();
   const fileId = router.query.fileId;
-  const page = router.query.page
-  const [pdfUrl, setPdfUrl] = useState<string>("")
+  const page = router.query.page;
+  const [pdfUrl, setPdfUrl] = useState<string>("");
 
   const getInvestorPdfData = async () => {
-    setPreload(true)
+    setPreload(true);
     if (typeof fileId === "string") {
       try {
-        const id = parseInt(fileId)
+        const id = parseInt(fileId);
         const url = await getInvestorFileFromBlob(id);
-        setPdfUrl(url)
+        setPdfUrl(url);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       } finally {
-        setPreload(false)
+        setPreload(false);
       }
     }
-
-  }
+  };
 
   const getArticlePdfData = async () => {
-    setPreload(true)
+    setPreload(true);
     if (typeof fileId === "string") {
       try {
-        const id = parseInt(fileId)
+        const id = parseInt(fileId);
         const url = await getArticleFileFromBlob(id);
-        setPdfUrl(url)
+        setPdfUrl(url);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       } finally {
-        setPreload(false)
+        setPreload(false);
       }
     }
-  }
+  };
 
   const getFinancialPdfData = async () => {
-    setPreload(true)
+    setPreload(true);
     if (typeof fileId === "string") {
       try {
-        const id = parseInt(fileId)
+        const id = parseInt(fileId);
         const url = await getFinancialFileFromBlob(id);
-        setPdfUrl(url)
+        setPdfUrl(url);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       } finally {
-        setPreload(false)
+        setPreload(false);
       }
     }
-  }
+  };
 
   const getFinancialMdaPdf = async () => {
-    setPreload(true)
+    setPreload(true);
     if (typeof fileId === "string") {
       try {
-        const id = parseInt(fileId)
+        const id = parseInt(fileId);
         const url = await getFinancialMdaFileFromBlob(id);
-        setPdfUrl(url)
+        setPdfUrl(url);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       } finally {
-        setPreload(false)
+        setPreload(false);
       }
     }
-  }
+  };
 
   const getNewsPdf = async () => {
-    setPreload(true)
+    setPreload(true);
     if (typeof fileId === "string") {
       try {
-        const id = parseInt(fileId)
+        const id = parseInt(fileId);
         const url = await getFileFromBlob(id);
-        setPdfUrl(url)
+        setPdfUrl(url);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       } finally {
-        setPreload(false)
+        setPreload(false);
       }
     }
-  }
+  };
 
   const getTechnicalReportsPdf = async () => {
-    setPreload(true)
+    setPreload(true);
     if (typeof fileId === "string") {
       try {
-        const id = parseInt(fileId)
+        const id = parseInt(fileId);
         const url = await getTechnicalFileFromBlob(id);
-        setPdfUrl(url)
+        setPdfUrl(url);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       } finally {
-        setPreload(false)
+        setPreload(false);
       }
     }
-  }
+  };
 
   useEffect(() => {
-    if(page === "investor-information"){
+    if (page === "investor-information") {
       getInvestorPdfData();
-    } else if(page === "articles") {
+    } else if (page === "articles") {
       getArticlePdfData();
-    } else if(page === "financial"){
+    } else if (page === "financial") {
       getFinancialPdfData();
-    } else if(page === "financial-mda"){
+    } else if (page === "financial-mda") {
       getFinancialMdaPdf();
-    } else if(page === "news"){
+    } else if (page === "news") {
       getNewsPdf();
-    } else if(page === "technical-report"){
+    } else if (page === "technical-report") {
       getTechnicalReportsPdf();
     }
-  }, [fileId, page])
+  }, [fileId, page]);
 
   return (
     <div className="h-screen bg-[#323639]">
       {preload ? (
-        <div
-          className="flex items-center justify-center h-screen"
-        >
-          <BarLoader
-            color="#F18628"
-            width={300}
-            height={4}
-          />
+        <div className="flex items-center justify-center h-screen">
+          <BarLoader color="#F18628" width={300} height={4} />
         </div>
+      ) : width >= 992 ? (
+        <>
+          <iframe width={"100%"} className="h-screen" src={pdfUrl}></iframe>
+        </>
       ) : (
         <>
-          <iframe width={"100%"} className="h-screen" src={pdfUrl}>
-          </iframe>
+          <button
+            onClick={() => window.open(pdfUrl)}
+            className="bg-[#25272A] font-hind text-primaryColor rounded-[3px] w-full h-full flex items-center justify-center font-semibold text-[22px] "
+          >
+            Download
+          </button>
         </>
       )}
     </div>
